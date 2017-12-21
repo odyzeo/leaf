f625006c3490e6cabaea969db0526452c64e0ff8
Tomáš Zamba <git@zombi.sk>
2017-12-21 16:04:16 +0100

    Add Banners news tags + Update shortcode to render banners by tags

--- wp-content/themes/themify-corporate-child/leaf/banners.php
+++ wp-content/themes/themify-corporate-child/leaf/banners.php
@@ -29,13 +29,48 @@ register_post_type( LEAF_POST_TYPE_BANNER, array(
 	'menu_icon'       => 'dashicons-format-image',
 ) );
 
+$labels = array(
+	'name'                       => __( 'Banner tags', 'leaf' ),
+	'singular_name'              => __( 'Banner tag', 'leaf' ),
+	'search_items'               => __( 'Search Banner tags', 'leaf' ),
+	'popular_items'              => __( 'Popular Banner tags', 'leaf' ),
+	'all_items'                  => __( 'All Banner tags', 'leaf' ),
+	'parent_item'                => null,
+	'parent_item_colon'          => null,
+	'edit_item'                  => __( 'Edit Banner tag', 'leaf' ),
+	'update_item'                => __( 'Update Banner tag', 'leaf' ),
+	'add_new_item'               => __( 'Add New Banner tag', 'leaf' ),
+	'new_item_name'              => __( 'New Banner tag Name', 'leaf' ),
+	'separate_items_with_commas' => __( 'Separate Banner tags with commas', 'leaf' ),
+	'add_or_remove_items'        => __( 'Add or remove Banner tags', 'leaf' ),
+	'choose_from_most_used'      => __( 'Choose from the most used Banner tags', 'leaf' ),
+	'not_found'                  => __( 'No Banner tags found.', 'leaf' ),
+	'menu_name'                  => __( 'Banner tags', 'leaf' ),
+);
+
+register_taxonomy( 'banner-tags',
+	array( 'banner' ),
+	array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'public'            => true,
+		'show_in_nav_menus' => true,
+		'show_ui'           => true,
+		'show_tagcloud'     => true,
+		'rewrite'           => true,
+	)
+);
+
 add_shortcode( 'banners', 'add_banner_shortcode' );
 
 function add_banner_shortcode( $atts ) {
 	$args = shortcode_atts( array(
-		'foo' => 'no foo',
-		'baz' => 'default baz',
+		'tags' => '',
 	), $atts );
+
+	$tags = explode( ',', $args['tags'] );
 
 	$post_type      = LEAF_POST_TYPE_BANNER;
 	$posts_per_page = 1;
@@ -44,7 +79,14 @@ function add_banner_shortcode( $atts ) {
 	$args   = array(
 		'posts_per_page' => $posts_per_page,
 		'post_type'      => $post_type,
-		'orderby'        => 'post_date'
+		'orderby'        => 'post_date',
+		'tax_query'      => array(
+			array(
+				'taxonomy' => 'banner-tags',
+				'field'    => 'slug',
+				'terms'    => $tags,
+			),
+		),
 	);
 
 	$wp_query = new WP_Query( $args );
