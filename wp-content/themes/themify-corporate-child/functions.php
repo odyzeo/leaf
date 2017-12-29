a1f27a01fdf20f5c3c4f6575c3186bdbffedf136
Tomáš Zamba <git@zombi.sk>
2017-12-29 11:21:21 +0100

    12840788 - Add latest-blogs shortcode with listing by category

--- wp-content/themes/themify-corporate-child/functions.php
+++ wp-content/themes/themify-corporate-child/functions.php
@@ -5,6 +5,7 @@ require_once( 'leaf/award-open-strip.php' );
 require_once( 'leaf/banners.php' );
 require_once( 'leaf/bubble.php' );
 require_once( 'leaf/stories.php' );
+require_once( 'leaf/latest-blogs.php' );
 require_once( 'leaf/latest-news.php' );
 require_once( 'leaf/news.php' );
 require_once( 'leaf/videostrip.php' );
---
+++ wp-content/themes/themify-corporate-child/leaf/latest-blogs.php
@@ -0,0 +1,64 @@
+<?php
+
+define( 'LEAF_POST_TYPE_BANNER', ' banner' );
+
+add_shortcode( 'latest-blogs', 'add_latest_blogs_shortcode' );
+
+function add_latest_blogs_shortcode( $atts ) {
+	$args = shortcode_atts( array(
+		'category' => '1', // Blog
+		'count'    => '3',
+		'display'  => 'inline',
+	), $atts );
+
+	$category__in = explode( ',', $args['category'] );
+
+	$post_type      = 'post';
+	$posts_per_page = (int) $args['count'];
+
+	$result = '';
+	$args   = array(
+		'posts_per_page' => $posts_per_page,
+		'post_type'      => $post_type,
+		'category__in' => $category__in
+	);
+
+	$wp_query = new WP_Query( $args );
+
+	$result .= "
+		<div class='flex flex--grid'>
+	";
+	if ( $wp_query->have_posts() ) {
+		while ( $wp_query->have_posts() ) {
+			$wp_query->the_post();
+
+			$title    = get_the_title();
+			$url      = get_the_permalink();
+			$datetime = get_the_date( 'c' );
+			$date     = get_the_date( 'd M Y' );
+			$content  = get_the_excerpt();
+
+			$result .= "
+				<div class='flex-1-3'>
+					<a href='$url' class='card'>
+						<div class='card__title'>$title</div>
+						<div class='card__perex'>$content</div>
+						<div class='card__meta'>
+							<time datetime='$datetime' itemprop='datePublished'>
+								$date
+							</time>
+						</div>
+					</a>
+				</div>
+			";
+		}
+	}
+
+	$result .= "
+		</div> 
+	";
+
+	wp_reset_query();
+
+	return $result;
+}
