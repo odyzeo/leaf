459e10b2c3577c6c11d68bd0f73b208e925f0816
Tomáš Zamba <git@zombi.sk>
2018-02-23 10:26:34 +0100

    Add PED custom post type

--- wp-content/themes/themify-corporate-child/leaf/ped/ped.php
+++ wp-content/themes/themify-corporate-child/leaf/ped/ped.php
@@ -50,35 +50,35 @@ define( 'LEAF_PED_OTHER', 'expertise-other' );
 define( 'LEAF_META_EXPERTISE', array(
     LEAF_PED_FINANCE   => array(
         'id'   => LEAF_PED_FINANCE,
-        'name' => __( 'Financie a biznis', 'workout' ),
+        'name' => __( 'Financie a biznis', 'PED' ),
     ),
     LEAF_PED_HR        => array(
         'id'   => LEAF_PED_HR,
-        'name' => __( 'Ľudské zdroje', 'workout' ),
+        'name' => __( 'Ľudské zdroje', 'PED' ),
     ),
     LEAF_PED_MARKETING => array(
         'id'   => LEAF_PED_MARKETING,
-        'name' => __( 'Marketing', 'workout' ),
+        'name' => __( 'Marketing', 'PED' ),
     ),
     LEAF_PED_SALES     => array(
         'id'   => LEAF_PED_SALES,
-        'name' => __( 'Predaj', 'workout' ),
+        'name' => __( 'Predaj', 'PED' ),
     ),
     LEAF_PED_LAW       => array(
         'id'   => LEAF_PED_LAW,
-        'name' => __( 'Právo', 'workout' ),
+        'name' => __( 'Právo', 'PED' ),
     ),
     LEAF_PED_IT        => array(
         'id'   => LEAF_PED_IT,
-        'name' => __( 'IT', 'workout' ),
+        'name' => __( 'IT', 'PED' ),
     ),
     LEAF_PED_ANALYSE   => array(
         'id'   => LEAF_PED_ANALYSE,
-        'name' => __( 'Analýza dát', 'workout' ),
+        'name' => __( 'Analýza dát', 'PED' ),
     ),
     LEAF_PED_OTHER     => array(
         'id'   => LEAF_PED_OTHER,
-        'name' => __( 'Iné', 'workout' ),
+        'name' => __( 'Iné', 'PED' ),
     ),
 ) );
 
@@ -113,39 +113,39 @@ define( 'LEAF_PED_LOCATION_KE', 'ke' );
 define( 'LEAF_META_LOCATION', array(
     LEAF_PED_LOCATION_HO => array(
         'id'   => LEAF_PED_LOCATION_HO,
-        'name' => __( 'Možné robiť aj na diaľku / zo zahraničia', 'workout' ),
+        'name' => __( 'Možné robiť aj na diaľku / zo zahraničia', 'PED' ),
     ),
     LEAF_PED_LOCATION_BA => array(
         'id'   => LEAF_PED_LOCATION_BA,
-        'name' => __( 'Bratislavský kraj', 'workout' ),
+        'name' => __( 'Bratislavský kraj', 'PED' ),
     ),
     LEAF_PED_LOCATION_TA => array(
         'id'   => LEAF_PED_LOCATION_TA,
-        'name' => __( 'Trnavský kraj', 'workout' ),
+        'name' => __( 'Trnavský kraj', 'PED' ),
     ),
     LEAF_PED_LOCATION_TT => array(
         'id'   => LEAF_PED_LOCATION_TT,
-        'name' => __( 'Trenčiansky kraj', 'workout' ),
+        'name' => __( 'Trenčiansky kraj', 'PED' ),
     ),
     LEAF_PED_LOCATION_NA => array(
         'id'   => LEAF_PED_LOCATION_NA,
-        'name' => __( 'Nitriansky kraj', 'workout' ),
+        'name' => __( 'Nitriansky kraj', 'PED' ),
     ),
     LEAF_PED_LOCATION_ZA => array(
         'id'   => LEAF_PED_LOCATION_ZA,
-        'name' => __( 'Žilinský kraj', 'workout' ),
+        'name' => __( 'Žilinský kraj', 'PED' ),
     ),
     LEAF_PED_LOCATION_BB => array(
         'id'   => LEAF_PED_LOCATION_BB,
-        'name' => __( 'Banskobystrický kraj', 'workout' ),
+        'name' => __( 'Banskobystrický kraj', 'PED' ),
     ),
     LEAF_PED_LOCATION_PO => array(
         'id'   => LEAF_PED_LOCATION_PO,
-        'name' => __( 'Prešovský kraj', 'workout' ),
+        'name' => __( 'Prešovský kraj', 'PED' ),
     ),
     LEAF_PED_LOCATION_KE => array(
         'id'   => LEAF_PED_LOCATION_KE,
-        'name' => __( 'Košický kraj', 'workout' ),
+        'name' => __( 'Košický kraj', 'PED' ),
     ),
 ) );
 
@@ -183,51 +183,51 @@ define( 'LEAF_PED_FOCUS_OTHER', 'focus-other' );
 define( 'LEAF_META_FOCUS', array(
     LEAF_PED_FOCUS_HUMANITARIAN => array(
         'id'   => LEAF_PED_FOCUS_HUMANITARIAN,
-        'name' => __( 'Humanitárna činnosť', 'workout' ),
+        'name' => __( 'Humanitárna činnosť', 'PED' ),
     ),
     LEAF_PED_FOCUS_CULTURE      => array(
         'id'   => LEAF_PED_FOCUS_CULTURE,
-        'name' => __( 'Kultúra', 'workout' ),
+        'name' => __( 'Kultúra', 'PED' ),
     ),
     LEAF_PED_FOCUS_DEVELOPMENT  => array(
         'id'   => LEAF_PED_FOCUS_DEVELOPMENT,
-        'name' => __( 'Rozvoj občianskej spoločnosti', 'workout' ),
+        'name' => __( 'Rozvoj občianskej spoločnosti', 'PED' ),
     ),
     LEAF_PED_FOCUS_LEARNING     => array(
         'id'   => LEAF_PED_FOCUS_LEARNING,
-        'name' => __( 'Vzdelávanie a rozvoj', 'workout' ),
+        'name' => __( 'Vzdelávanie a rozvoj', 'PED' ),
     ),
     LEAF_PED_FOCUS_SOCIAL       => array(
         'id'   => LEAF_PED_FOCUS_SOCIAL,
-        'name' => __( 'Sociálne znevýhodnení občania', 'workout' ),
+        'name' => __( 'Sociálne znevýhodnení občania', 'PED' ),
     ),
     LEAF_PED_FOCUS_HEALTH       => array(
         'id'   => LEAF_PED_FOCUS_HEALTH,
-        'name' => __( 'Zdravotne znevýhodnení občania', 'workout' ),
+        'name' => __( 'Zdravotne znevýhodnení občania', 'PED' ),
     ),
     LEAF_PED_FOCUS_DEV_SK       => array(
         'id'   => LEAF_PED_FOCUS_DEV_SK,
-        'name' => __( 'Rozvoj SR', 'workout' ),
+        'name' => __( 'Rozvoj SR', 'PED' ),
     ),
     LEAF_PED_FOCUS_SENIORS      => array(
         'id'   => LEAF_PED_FOCUS_SENIORS,
-        'name' => __( 'Seniori', 'workout' ),
+        'name' => __( 'Seniori', 'PED' ),
     ),
     LEAF_PED_FOCUS_ENV          => array(
         'id'   => LEAF_PED_FOCUS_ENV,
-        'name' => __( 'Životné prostredie', 'workout' ),
+        'name' => __( 'Životné prostredie', 'PED' ),
     ),
     LEAF_PED_FOCUS_SCIENCE      => array(
         'id'   => LEAF_PED_FOCUS_SCIENCE,
-        'name' => __( 'Veda a technológie', 'workout' ),
+        'name' => __( 'Veda a technológie', 'PED' ),
     ),
     LEAF_PED_FOCUS_START_UP     => array(
         'id'   => LEAF_PED_FOCUS_START_UP,
-        'name' => __( 'Start up', 'workout' ),
+        'name' => __( 'Start up', 'PED' ),
     ),
     LEAF_PED_FOCUS_OTHER        => array(
         'id'   => LEAF_PED_FOCUS_OTHER,
-        'name' => __( 'Iné', 'workout' ),
+        'name' => __( 'Iné', 'PED' ),
     ),
 ) );
 
@@ -247,15 +247,15 @@ define( 'LEAF_PED_KIND_START_UP', 'kind-start-up' );
 define( 'LEAF_META_KIND', array(
     LEAF_PED_KIND_NON_PROFIT => array(
         'id'   => LEAF_PED_KIND_NON_PROFIT,
-        'name' => __( 'Nezisková organizácia', 'workout' ),
+        'name' => __( 'Nezisková organizácia', 'PED' ),
     ),
     LEAF_PED_KIND_SOCIAL     => array(
         'id'   => LEAF_PED_KIND_SOCIAL,
-        'name' => __( 'Sociálny podnik', 'workout' ),
+        'name' => __( 'Sociálny podnik', 'PED' ),
     ),
     LEAF_PED_KIND_START_UP   => array(
         'id'   => LEAF_PED_KIND_START_UP,
-        'name' => __( 'Start-up', 'workout' ),
+        'name' => __( 'Start-up', 'PED' ),
     ),
 ) );
 
@@ -277,19 +277,19 @@ define( 'LEAF_PED_PERIOD_12', 'period-12' );
 define( 'LEAF_META_PERIOD', array(
     LEAF_PED_PERIOD_3  => array(
         'id'   => LEAF_PED_PERIOD_3,
-        'name' => __( '3 mesiace', 'workout' ),
+        'name' => __( '3 mesiace', 'PED' ),
     ),
     LEAF_PED_PERIOD_6  => array(
         'id'   => LEAF_PED_PERIOD_6,
-        'name' => __( '6 mesiacov', 'workout' ),
+        'name' => __( '6 mesiacov', 'PED' ),
     ),
     LEAF_PED_PERIOD_9  => array(
         'id'   => LEAF_PED_PERIOD_9,
-        'name' => __( '9 mesiasov', 'workout' ),
+        'name' => __( '9 mesiasov', 'PED' ),
     ),
     LEAF_PED_PERIOD_12 => array(
         'id'   => LEAF_PED_PERIOD_12,
-        'name' => __( '12 mesiacov', 'workout' ),
+        'name' => __( '12 mesiacov', 'PED' ),
     ),
 ) );
 
@@ -323,44 +323,94 @@ define( 'LEAF_PED_WEB', 'ped_web' );
 define( 'LEAF_META_TEXTS', array(
     LEAF_PED_CHALLENGE       => array(
         'id'   => LEAF_PED_CHALLENGE,
-        'name' => __( 'Výzva', 'workout' ),
+        'name' => __( 'Výzva', 'PED' ),
     ),
     LEAF_PED_EXCERPT         => array(
         'id'   => LEAF_PED_EXCERPT,
-        'name' => __( 'Zhrnutie', 'workout' ),
+        'name' => __( 'Zhrnutie', 'PED' ),
     ),
     LEAF_PED_CITY            => array(
         'id'   => LEAF_PED_CITY,
-        'name' => __( 'Mesto', 'workout' ),
+        'name' => __( 'Mesto', 'PED' ),
     ),
     LEAF_PED_HOURS_ESTIMATED => array(
         'id'   => LEAF_PED_HOURS_ESTIMATED,
-        'name' => __( 'Približný počet hodín na týždeň', 'workout' ),
+        'name' => __( 'Približný počet hodín na týždeň', 'PED' ),
     ),
     LEAF_PED_ABOUT           => array(
         'id'   => LEAF_PED_ABOUT,
-        'name' => __( 'O organizácii', 'workout' ),
+        'name' => __( 'O organizácii', 'PED' ),
     ),
     LEAF_PED_WEB             => array(
         'id'   => LEAF_PED_WEB,
-        'name' => __( 'Webstránka', 'workout' ),
+        'name' => __( 'Webstránka', 'PED' ),
     ),
 ) );
 
 define( 'LEAF_META_WYSIWYG', array(
     LEAF_PED_OUTPUT  => array(
         'id'   => LEAF_PED_OUTPUT,
-        'name' => __( 'Výstup', 'workout' ),
+        'name' => __( 'Výstup', 'PED' ),
     ),
     LEAF_PED_PROFILE => array(
         'id'   => LEAF_PED_PROFILE,
-        'name' => __( 'Profil dobrovoľníka', 'workout' ),
+        'name' => __( 'Profil dobrovoľníka', 'PED' ),
     ),
     LEAF_PED_WHY     => array(
         'id'   => LEAF_PED_WHY,
-        'name' => __( 'Prečo by si si nás mal vybrať', 'workout' ),
+        'name' => __( 'Prečo by si si nás mal vybrať', 'PED' ),
     ),
 ) );
+
+
+/**
+ * Register POST TYPE
+ */
+add_action( 'init', 'leaf_register_ped_post_type' );
+
+function leaf_register_ped_post_type() {
+
+    /**
+     * Post Type: PEDs.
+     */
+
+    $labels = array(
+        "name"               => __( "PED 8s", "PED 8" ),
+        "singular_name"      => __( "PED 8", "PED 8" ),
+        'add_new'            => __( 'Add PED 8' ),
+        'all_items'          => __( 'All PED 8s' ),
+        'add_new_item'       => __( 'Add PED 8' ),
+        'edit_item'          => __( 'Edit PED 8' ),
+        'new_item'           => __( 'New PED 8' ),
+        'view_item'          => __( 'View PED 8' ),
+        'search_items'       => __( 'Search PED 8s' ),
+        'not_found'          => __( 'No PED 8s found' ),
+        'not_found_in_trash' => __( 'No PED 8s found in trash' ),
+        'parent_item_colon'  => __( 'Parent PED 8' ),
+    );
+
+    $args = array(
+        "label"               => __( "PED 8s", "PED 8" ),
+        "labels"              => $labels,
+        "description"         => "",
+        "public"              => true,
+        "publicly_queryable"  => true,
+        "show_ui"             => true,
+        "show_in_rest"        => true,
+        "rest_base"           => "PED 8",
+        "has_archive"         => true,
+        "show_in_menu"        => true,
+        "exclude_from_search" => false,
+        "hierarchical"        => false,
+        "rewrite"             => false,
+        "query_var"           => true,
+        "menu_position"       => 22,
+        "supports"            => array( "title", "editor", "thumbnail" ),
+        'menu_icon'           => 'dashicons-heart',
+    );
+
+    register_post_type( LEAF_POST_TYPE_PED, $args );
+}
 
 require_once( LEAF_PLUGIN_DIR . 'class.ped-meta-box.php' );
 new Ped_Meta_Box();
@@ -443,7 +493,7 @@ add_action( "admin_notices", function () {
         echo "<div class='updated'>";
         echo "<p>";
         echo "To insert the posts into the database, set FILE ID and click the button below.";
-        echo "<form action='{$_SERVER["REQUEST_URI"]}&insert_sitepoint_posts' method='GET'>";
+        echo "<form action='{$_SERVER["REQUEST_URI"]}&insert_ped_posts' method='GET'>";
         echo "<input type='hidden' name='post_type' value='ped-8'>";
         echo "<input type='hidden' name='insert_sitepoint_posts' value='true'>";
         echo "<input name='ped-upload'>";
@@ -461,7 +511,7 @@ add_action( "admin_init", function () {
 
     // I'd recommend replacing this with your own code to make sure
     //  the post creation _only_ happens when you want it to.
-    if ( ! isset( $_GET["insert_sitepoint_posts"] ) ) {
+    if ( ! isset( $_GET["insert_ped_posts"] ) ) {
         return;
     }
 
@@ -599,3 +649,4 @@ add_action( "admin_init", function () {
     }
 
 } );
+
