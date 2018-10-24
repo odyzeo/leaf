817cd575fef06a52a1732c9c355932927f617673
Tomáš Zamba <tomas.zamba@asseco-ce.com>
2018-10-24 14:12:58 +0200

    Generate production assets

--- wp-content/themes/themify-corporate-child/assets/js/components/Swipers.js
+++ wp-content/themes/themify-corporate-child/assets/js/components/Swipers.js
@@ -1,28 +1,27 @@
-import ProgressBar from 'progressbar.js'
-import Swiper from 'swiper'
+import ProgressBar from 'progressbar.js';
+import Swiper from 'swiper';
 
 const Swipers = {
     init() {
-        this.initNews()
-        this.initStories()
+        this.initNews();
+        this.initStories();
     },
     initNews() {
-    /**
-     * Swiper news
-     */
-        const $newsSwipers = $('.js-swiper-news')
+        /**
+         * Swiper news
+         */
+        const $newsSwipers = $('.js-swiper-news');
         if ($newsSwipers.length > 0) {
             $newsSwipers.each(function () {
-                const autoplayTime = Number($(this).data('autoplay'))
-                const $newsSwiper = $(this)
-                const newsCount = $newsSwiper.find('.swiper-slide').length
-                $newsSwiper.toggleClass('swiper-container--single', newsCount === 1)
+                const autoplayTime = +$(this).data('autoplay');
+                const $newsSwiper = $(this);
+                const newsCount = $newsSwiper.find('.swiper-slide').length;
+                $newsSwiper.toggleClass('swiper-container--single', newsCount === 1);
 
                 const defaultNewsOptions = {
                     loop: true,
                     slidesPerView: 2,
                     spaceBetween: 10,
-
                     pagination: {
                         el: '.js-swiper-news-pagination',
                         type: 'bullets',
@@ -31,10 +30,6 @@ const Swipers = {
                         nextEl: '.js-swiper-news-next',
                         prevEl: '.js-swiper-news-prev',
                     },
-                    autoplay: {
-                        delay: autoplayTime,
-                        disableOnInteraction: false,
-                    },
                     breakpoints: {
                         // when window width is <= 640px
                         640: {
@@ -42,41 +37,44 @@ const Swipers = {
                             spaceBetween: 20,
                         },
                     },
-                }
+                };
 
-                if (autoplayTime === 0) {
-                    defaultNewsOptions.autoplay = false
+                if (!Number.isNaN(autoplayTime) && autoplayTime > 0) {
+                    defaultNewsOptions.autoplay = {
+                        delay: autoplayTime,
+                        disableOnInteraction: true,
+                    };
                 }
 
                 if (newsCount === 1) {
-                    defaultNewsOptions.loop = false
-                    defaultNewsOptions.slidesPerView = 1
-                    defaultNewsOptions.centeredSlides = true
-                    defaultNewsOptions.spaceBetween = 0
-                    defaultNewsOptions.navigation = false
-                    defaultNewsOptions.pagination = false
+                    defaultNewsOptions.loop = false;
+                    defaultNewsOptions.slidesPerView = 1;
+                    defaultNewsOptions.centeredSlides = true;
+                    defaultNewsOptions.spaceBetween = 0;
+                    defaultNewsOptions.navigation = false;
+                    defaultNewsOptions.pagination = false;
                 }
 
-                const $swiper = new Swiper($newsSwiper, defaultNewsOptions)
-            })
+                const $swiper = new Swiper($newsSwiper, defaultNewsOptions);
+            });
         }
     },
     initStories() {
-    /**
-     * Swiper stories
-     */
-        const $storiesCirclesSwiper = $('.js-swiper-stories-circles')
-        const $storiesCirclesSwiperSlides = $('.js-swiper-stories-circles').find('.swiper-slide')
-        const $storiesSwiper = $('.js-swiper-stories')
-        const storiesCount = $storiesSwiper.find('.swiper-slide:not(.swiper-slide-duplicate)').length
+        /**
+         * Swiper stories
+         */
+        const $storiesCirclesSwiper = $('.js-swiper-stories-circles');
+        const $storiesCirclesSwiperSlides = $('.js-swiper-stories-circles').find('.swiper-slide');
+        const $storiesSwiper = $('.js-swiper-stories');
+        const storiesCount = $storiesSwiper.find('.swiper-slide:not(.swiper-slide-duplicate)').length;
 
         // Scroll to swiper stories when click on
         $storiesCirclesSwiper.find('.swiper-slide').on('click', () => {
             // Smaller header height 70 - desktop, 60 - desktop smaller, mobile should be small
             // - 10 for offset
-            const top = $('#swiper-circles').offset().top - Math.min($('.js-header').height(), 60) - 10
-            $('html, body').animate({ scrollTop: `${top}px` }, 300)
-        })
+            const top = $('#swiper-circles').offset().top - Math.min($('.js-header').height(), 60) - 10;
+            $('html, body').animate({ scrollTop: `${top}px` }, 300);
+        });
 
         if ($storiesSwiper.length > 0) {
             const defaultCirclesOptions = {
@@ -91,17 +89,17 @@ const Swipers = {
                 on: {
                     slideChangeTransitionEnd: transitionEndCircles,
                 },
-            }
+            };
 
-            const looping = ($storiesCirclesSwiperSlides.length > 10)
+            const looping = ($storiesCirclesSwiperSlides.length > 10);
             if (looping) {
-                defaultCirclesOptions.loop = true
-                defaultCirclesOptions.loopedSlides = 50
-                defaultCirclesOptions.loopAdditionalSlides = 50
+                defaultCirclesOptions.loop = true;
+                defaultCirclesOptions.loopedSlides = 50;
+                defaultCirclesOptions.loopAdditionalSlides = 50;
             }
 
-            const speed = 300
-            const autoplayTime = 15000
+            const speed = 300;
+            const autoplayTime = 15000;
             const defaultOptions = {
                 slidesPerView: 1,
                 speed,
@@ -119,13 +117,13 @@ const Swipers = {
                 on: {
                     slideChangeTransitionEnd: transitionEnd,
                 },
-            }
+            };
 
-            let initialized = false
-            let clicked = false
+            let initialized = false;
+            let clicked = false;
 
-            const $swiper = new Swiper($storiesSwiper, defaultOptions)
-            const $circlesSwiper = new Swiper($storiesCirclesSwiper, defaultCirclesOptions)
+            const $swiper = new Swiper($storiesSwiper, defaultOptions);
+            const $circlesSwiper = new Swiper($storiesCirclesSwiper, defaultCirclesOptions);
 
             // if (!looping) {
             //   console.log('bind')
@@ -146,14 +144,14 @@ const Swipers = {
 
             function transitionEndCircles() {
                 /**
-         * Progress bar
-         */
-                const $active = $('.js-swiper-stories-circles .swiper-slide-active .js-swiper-timer')
-                let $progress = $('#js-swiper-progress')
-                $progress.remove()
-                $progress = $('<div id=\'js-swiper-progress\' class=\'swiper__progress\'></div>')
+                 * Progress bar
+                 */
+                const $active = $('.js-swiper-stories-circles .swiper-slide-active .js-swiper-timer');
+                let $progress = $('#js-swiper-progress');
+                $progress.remove();
+                $progress = $('<div id=\'js-swiper-progress\' class=\'swiper__progress\'></div>');
 
-                $progress.appendTo($active)
+                $progress.appendTo($active);
                 const bar = new ProgressBar.Circle('#js-swiper-progress', {
                     strokeWidth: 3,
                     easing: 'linear',
@@ -162,44 +160,44 @@ const Swipers = {
                     trailColor: '#fff',
                     trailWidth: 2,
                     svgStyle: null,
-                })
-                bar.animate(1.0)
+                });
+                bar.animate(1.0);
 
                 /**
-         * Prevent slide from another swiper
-         */
+                 * Prevent slide from another swiper
+                 */
                 if (clicked) {
-                    clicked = false
-                    return
+                    clicked = false;
+                    return;
                 }
 
-                let clickedIndex = this.realIndex
+                let clickedIndex = this.realIndex;
                 if ($swiper && initialized) {
-                    clickedIndex %= storiesCount// + 1
-                    clicked = true
-                    $swiper.slideTo(clickedIndex)
+                    clickedIndex %= storiesCount;// + 1
+                    clicked = true;
+                    $swiper.slideTo(clickedIndex);
                 }
-                initialized = true
+                initialized = true;
             }
 
             function transitionEnd() {
                 /**
-         * Prevent slide from another swiper
-         */
+                 * Prevent slide from another swiper
+                 */
                 if (clicked) {
-                    clicked = false
-                    return
+                    clicked = false;
+                    return;
                 }
 
-                const index = this.realIndex
+                const index = this.realIndex;
                 if ($circlesSwiper) {
-                    clicked = true
-                    const to = looping ? index + storiesCount : index
-                    $circlesSwiper.slideTo(to)
+                    clicked = true;
+                    const to = looping ? index + storiesCount : index;
+                    $circlesSwiper.slideTo(to);
                 }
             }
         }
     },
-}
+};
 
-export default Swipers
+export default Swipers;
